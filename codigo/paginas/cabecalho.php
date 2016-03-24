58dc621a27308c4033eceab4bdb5f72bcd189d92
Victor Maciel de Oliveira <executnoob@gmail.com>
2016-03-24 09:33:07 -0300

    Commit

--- codigo/paginas/cabecalho.php
+++ codigo/paginas/cabecalho.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="../mdl/material.min.css">
-    <script src="../mdl/material.min.js"></script>
-    <link rel="stylesheet" href="../modal/remodal.css">
-    <link rel="stylesheet" href="../modal/remodal-default-theme.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link href="../select/select2.min.css" rel="stylesheet"/>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="../mdl/material.min.css">
+  <script src="../mdl/material.min.js"></script>
+  <link rel="stylesheet" href="../modal/remodal.css">
+  <link rel="stylesheet" href="../modal/remodal-default-theme.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link href="../select/select2.min.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="../modal/sweetalert2.css">
 
-  </head>
-  <body>
-    <script src="../jquery.min.js"></script>
-    <script src="../select/select2.min.js"></script>
-    <script src="../modal/remodal.min.js"></script>
-    <script type="text/javascript">
-    $(document).ready(function() {
-      $(".js-example-basic-single").select2();
-    });
-</script>
-    </body>
+  
+</head>
+<body>
+  <script src="../jquery.min.js"></script>
+  <script src="../select/select2.min.js"></script>
+  <script src="../modal/remodal.min.js"></script>
+  <script src="../modal/sweetalert2.min.js"></script>
+  <script type="text/javascript">
+  $(document).ready(function() {
+    $(".js-example-basic-single").select2();
+  });
+  </script>
+</body>
 </html>
